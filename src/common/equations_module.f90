cc6710296eb9268742149f52d5138e71833593c3
Seiya Nishizawa <seiya@gfd-dennou.org>
2025-08-07 11:17:55 +0900

    Switch to Cartesian channel with free-slip

--- src/common/equations_module.f90
+++ src/common/equations_module.f90
@@ -1,23 +1,21 @@
 module equations_module
   use constants_module, only: dp
-  use variables_module, only: nlon, nlat, pi, radius, dlon, dlat, h0, h1, omega, g
+  use variables_module, only: nlon, nlat, dx, dy, g, f0, h0, h1, pi
   implicit none
-
 contains
 
-  !$FAD CONSTANT_VARS: lon, lat
-  subroutine init_height(h, lon, lat)
+  subroutine init_height(h, x, y)
     real(dp), intent(out) :: h(nlon,nlat)
-    real(dp), intent(in) :: lon(nlon), lat(nlat)
-    real(dp) :: lambda0, phi0, r0, dist
+    real(dp), intent(in) :: x(nlon), y(nlat)
+    real(dp) :: x0, y0, r0, dist
     integer :: i,j
-    lambda0 = 3.d0*pi/2.d0
-    phi0 = 0.d0
-    r0 = radius/3.d0
+    x0 = 1.5d0*dx*nlon
+    y0 = 0.5d0*dy*nlat
+    r0 = dy*nlat/3.d0
     do j=1,nlat
        do i=1,nlon
           h(i,j) = h0
-          call gc_distance(lon(i),lat(j),lambda0,phi0,dist)
+          dist = sqrt( (x(i)-x0)**2 + (y(j)-y0)**2 )
           if (dist < r0) then
              h(i,j) = h0 + 0.5d0*h1*(1.d0+cos(pi*dist/r0))
           end if
@@ -25,98 +23,32 @@ contains
     end do
   end subroutine init_height
 
-  !$FAD CONSTANT_VARS: lon, lat, alpha
-  subroutine velocity_field(u,v,lon,lat,alpha)
+  subroutine velocity_field(u,v,x,y,alpha)
     real(dp), intent(out) :: u(nlon,nlat), v(nlon,nlat+1)
-    real(dp), intent(in) :: lon(nlon), lat(nlat), alpha
-    real(dp) :: u0, lon_edge
+    real(dp), intent(in) :: x(nlon), y(nlat), alpha
     integer :: i,j
-    u0 = omega*radius
-    do j=1,nlat
-       do i=1,nlon
-          lon_edge = (i-1)*dlon
-          u(i,j) = u0*(cos(lat(j))*cos(alpha) + sin(lat(j))*cos(lon_edge)*sin(alpha))
-       end do
-    end do
-    do j=1,nlat+1
-       do i=1,nlon
-          v(i,j) = -u0*sin(lon(i))*sin(alpha)
-       end do
-    end do
+    u = 0.d0
+    v = 0.d0
     v(:,1) = 0.d0
     v(:,nlat+1) = 0.d0
   end subroutine velocity_field
 
-  !$FAD SKIP
-  subroutine analytic_height(ha, lon, lat, t, alpha)
+  subroutine analytic_height(ha, x, y, t, alpha)
     real(dp), intent(out) :: ha(nlon,nlat)
-    real(dp), intent(in) :: lon(nlon), lat(nlat), t, alpha
-    real(dp) :: lonr, latr, theta
-    integer :: i,j
-    theta = -omega*t
-    do j=1,nlat
-       do i=1,nlon
-          call rotate_point(lon(i),lat(j),alpha,theta,lonr,latr)
-          ha(i,j) = initial_at_point(lonr,latr)
-       end do
-    end do
+    real(dp), intent(in) :: x(nlon), y(nlat), t, alpha
+    call init_height(ha, x, y)
   end subroutine analytic_height
 
-  !$FAD CONSTANT_VARS: lon, lat
-  function initial_at_point(lon,lat) result(hp)
-    real(dp), intent(in) :: lon, lat
-    real(dp) :: hp, lambda0, phi0, r0, dist
-    lambda0 = 3.d0*pi/2.d0
-    phi0 = 0.d0
-    r0 = radius/3.d0
-    hp = h0
-    call gc_distance(lon,lat,lambda0,phi0,dist)
-    if (dist < r0) hp = h0 + 0.5d0*h1*(1.d0+cos(pi*dist/r0))
-  end function initial_at_point
-
-  !$FAD SKIP
-  subroutine gc_distance(lon1,lat1,lon2,lat2,dist)
-    real(dp), intent(in) :: lon1,lat1,lon2,lat2
-    real(dp), intent(out) :: dist
-    dist = radius*acos( sin(lat1)*sin(lat2) + cos(lat1)*cos(lat2)*cos(lon1-lon2) )
-  end subroutine gc_distance
-
-  !$FAD SKIP
-  subroutine rotate_point(lon,lat,alpha,theta,lonp,latp)
-    real(dp), intent(in) :: lon, lat, alpha, theta
-    real(dp), intent(out) :: lonp, latp
-    real(dp) :: x,y,z,xp,yp,zp
-    real(dp) :: n1,n2,n3,dot,c1,c2,c3,ct,st
-    x = cos(lat)*cos(lon)
-    y = cos(lat)*sin(lon)
-    z = sin(lat)
-    n1 = -sin(alpha)
-    n2 = 0.d0
-    n3 = cos(alpha)
-    ct = cos(theta)
-    st = sin(theta)
-    dot = n1*x + n2*y + n3*z
-    c1 = n2*z - n3*y
-    c2 = n3*x - n1*z
-    c3 = n1*y - n2*x
-    xp = x*ct + c1*st + n1*dot*(1.d0-ct)
-    yp = y*ct + c2*st + n2*dot*(1.d0-ct)
-    zp = z*ct + c3*st + n3*dot*(1.d0-ct)
-    lonp = atan2(yp,xp)
-    if (lonp < 0.d0) lonp = lonp + 2.d0*pi
-    latp = asin(zp)
-  end subroutine rotate_point
-
-  !$FAD CONSTANT_VARS: lat, no_momentum_tendency
-  subroutine rhs(h, u, v, dhdt, dudt, dvdt, lat, no_momentum_tendency)
+  subroutine rhs(h, u, v, dhdt, dudt, dvdt, no_momentum_tendency)
     real(dp), intent(in) :: h(nlon,nlat), u(nlon,nlat), v(nlon,nlat+1)
     real(dp), intent(out) :: dhdt(nlon,nlat)
     real(dp), intent(out) :: dudt(nlon,nlat), dvdt(nlon,nlat+1)
-    real(dp), intent(in) :: lat(nlat)
     logical, intent(in), optional :: no_momentum_tendency
     integer :: i,j,ip1,im1,jp1,jm1
     real(dp) :: fe,fw,fn,fs,ue,uw,vn,vs
-    real(dp) :: fcor, h_e, h_w, h_n, h_s, v_avg, u_avg
+    real(dp) :: h_e, h_w, h_n, h_s, v_avg, u_avg
+    real(dp) :: fcor
+
     ! continuity equation
     do j=1,nlat
        jp1 = min(j+1,nlat)
@@ -136,8 +68,8 @@ contains
           else
              fw = uw * h(i,j)
           end if
-          vn = v(i,j+1) * cos((lat(j) + lat(jp1)) * 0.5d0)
-          vs = v(i,j) * cos((lat(jm1) + lat(j)) * 0.5d0)
+          vn = v(i,j+1)
+          vs = v(i,j)
           if (vn > 0.d0) then
              fn = vn * h(i,j)
           else
@@ -148,7 +80,7 @@ contains
           else
              fs = vs * h(i,j)
           end if
-          dhdt(i,j) = -( (fe - fw)/dlon + (fn - fs)/dlat ) / (radius * cos(lat(j)))
+          dhdt(i,j) = -((fe - fw)/dx + (fn - fs)/dy)
        end do
     end do
 
@@ -160,40 +92,38 @@ contains
        end if
     end if
 
+    fcor = f0
+
     ! zonal momentum
     do j=1,nlat
-       jp1 = mod(j,nlat)+1
-       jm1 = mod(j-2+nlat,nlat)+1
-       fcor = 2.d0*omega*sin(lat(j))
+       jp1 = min(j+1,nlat)
+       jm1 = max(j-1,1)
        do i=1,nlon
-         ip1 = mod(i,nlon)+1
-         im1 = mod(i-2+nlon,nlon)+1
+          ip1 = mod(i,nlon)+1
+          im1 = mod(i-2+nlon,nlon)+1
           h_e = h(i,j)
           h_w = h(im1,j)
           v_avg = 0.25d0*(v(im1,j) + v(im1,j+1) + v(i,j) + v(i,j+1))
-          dudt(i,j) = - u(i,j) * (u(ip1,j) - u(im1,j)) / (2.0d0 * dlon * radius * cos(lat(j))) &
-                      - v_avg * (u(i,jp1) - u(i,jm1)) / (2.0d0 * dlat * radius) &
-                      + u(i,j) * v_avg * tan(lat(j)) / radius &
-                      - g * (h_e - h_w) / (dlon * radius * cos(lat(j))) &
+          dudt(i,j) = - u(i,j) * (u(ip1,j) - u(im1,j)) / (2.d0*dx) &
+                      - v_avg * (u(i,jp1) - u(i,jm1)) / (2.d0*dy) &
+                      - g * (h_e - h_w) / dx &
                       + fcor * v_avg
        end do
     end do
 
     ! meridional momentum
     do j=2,nlat
-        jp1 = j+1
-        jm1 = j-1
-        fcor = 2.d0*omega*sin(-pi/2.d0 + (j-1)*dlat)
-        do i=1,nlon
+       jp1 = min(j+1,nlat)
+       jm1 = j-1
+       do i=1,nlon
           ip1 = mod(i,nlon)+1
           im1 = mod(i-2+nlon,nlon)+1
           h_n = h(i,j)
           h_s = h(i,jm1)
           u_avg = 0.25d0*(u(i,jm1) + u(ip1,jm1) + u(i,j) + u(ip1,j))
-          dvdt(i,j) = - u_avg * (v(ip1,j) - v(im1,j)) / (2.0d0 * dlon * radius * cos(lat(j))) &
-                      - v(i,j) * (v(i,jp1) - v(i,jm1)) / (2.0d0 * dlat * radius) &
-                      - u_avg**2 * tan(lat(j)) / radius &
-                      - g * (h_n - h_s) / (dlat * radius) &
+          dvdt(i,j) = - u_avg * (v(ip1,j) - v(im1,j)) / (2.d0*dx) &
+                      - v(i,j) * (v(i,jp1) - v(i,jm1)) / (2.d0*dy) &
+                      - g * (h_n - h_s) / dy &
                       - fcor * u_avg
        end do
     end do
--- src/common/variables_module.f90
+++ src/common/variables_module.f90
@@ -3,44 +3,44 @@ module variables_module
   implicit none
   integer, parameter :: nlon=128, nlat=64
   real(dp), parameter :: pi=3.14159265358979323846d0
-  real(dp), parameter :: radius=6371220.d0
   real(dp), parameter :: g=9.80616d0
   real(dp), parameter :: day=86400.d0
-  real(dp), parameter :: dlon=2.d0*pi/nlon, dlat=pi/nlat
+  real(dp), parameter :: Lx=2.d0*pi, Ly=1.d0
+  real(dp), parameter :: dx=Lx/nlon, dy=Ly/nlat
   real(dp), parameter :: h0=10000.d0, h1=2000.d0
-  real(dp) :: omega=2.d0*pi/(12.d0*day)
+  real(dp), parameter :: f0=1.0d-4
   real(dp), parameter :: dt=600.d0
   integer, parameter :: nsteps=nint(12.d0*day/dt)
   integer :: output_interval=48
-  real(dp), allocatable :: lon(:), lat(:)
+  real(dp), allocatable :: x(:), y(:)
   real(dp), allocatable :: h(:,:), hn(:,:), ha(:,:)
   real(dp), allocatable :: u(:,:), v(:,:)
   real(sp), allocatable :: hsp(:,:), usp(:,:), vsp(:,:)
 
-  !$FAD CONSTANT_VARS: lon, lat
+  !$FAD CONSTANT_VARS: x, y
   !$FAD CONSTANT_VARS: ha
   !$FAD CONSTANT_VARS: hsp, usp, vsp
-  !$FAD CONSTANT_VARS: omega
+  !$FAD CONSTANT_VARS: f0
 
 contains
 
   subroutine init_variables()
     integer :: i, j
-    allocate(lon(nlon), lat(nlat))
+    allocate(x(nlon), y(nlat))
     allocate(h(nlon,nlat), hn(nlon,nlat), ha(nlon,nlat))
     allocate(u(nlon,nlat), v(nlon,nlat+1))
     allocate(hsp(nlon,nlat), usp(nlon,nlat), vsp(nlon,nlat))
     do i=1,nlon
-       lon(i) = (i-0.5d0)*dlon
+       x(i) = (i-0.5d0)*dx
     end do
     do j=1,nlat
-       lat(j) = -pi/2.d0 + (j-0.5d0)*dlat
+       y(j) = (j-0.5d0)*dy
     end do
   end subroutine init_variables
 
   subroutine finalize_variables()
-    if (allocated(lon)) deallocate(lon)
-    if (allocated(lat)) deallocate(lat)
+    if (allocated(x)) deallocate(x)
+    if (allocated(y)) deallocate(y)
     if (allocated(h)) deallocate(h)
     if (allocated(hn)) deallocate(hn)
     if (allocated(ha)) deallocate(ha)
